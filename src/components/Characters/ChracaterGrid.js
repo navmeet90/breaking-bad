26c5a768b515f0ad6de6729f370f7c9f1e8ee7ba
Navmeet <navmeet.ratan@technossus.com>
2022-07-15 20:10:04 +0530

    partial

---
+++ src/components/Characters/ChracaterGrid.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Spinner from "../ui/Spinner";
+import CharacterItem from "./CharacterItem";
+
+const ChracaterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
+      ))}
+    </section>
+  );
+};
+
+export default ChracaterGrid;
